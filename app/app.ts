9f9b9794f4a22b333eb4f72b45aecc6070c1a770
Michael Upton <uptonm@wit.edu>
2019-04-02 09:03:04 -0400

    fixing cookie session

--- app/app.ts
+++ app/app.ts
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIEKEY || 'lsajkndlsakndlaskndlaskndlaskndl11112134124sadas']
+    keys: [process.env.COOKIEKEY || '']
   })
 );
 app.use(passport.initialize());
